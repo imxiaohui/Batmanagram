90d524ecb4a092cf634188d7f7b2379fe6b756b6
Sebastian Herbermann <sebastian.herbermann@googlemail.com>
2012-09-19 15:17:28 +0100

    initial import

---
+++ StarTrekWordLibrary/src/com/toy/anagrams/words/startrek/BatmanWordLibrary.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2010, Oracle.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the distribution.
+ *  * Neither the name of Oracle nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ */
+
+/* Anagram Game Application */
+
+package com.toy.anagrams.words.startrek;
+
+import com.toy.anagrams.api.WordLibrary;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ * Implementation of the logic for the Anagram Game application.
+ */
+@ServiceProvider(service=WordLibrary.class)
+final public class BatmanWordLibrary implements WordLibrary {
+
+    private static final String[] WORD_LIST = {
+        "Kirk",
+        "Enterprise",
+        "Voyager",
+        "Borg",
+        "Klingone",
+        "Janeway",
+        "Scotty",
+        "Pille"
+    };
+    
+
+    /**
+     * Singleton class.
+     */
+    public BatmanWordLibrary() {
+    }
+    
+    @Override
+    public String toString() {
+        return "Words from Star Trek";
+    }
+
+    /**
+     * Gets the word at a given index.
+     * @param idx index of required word
+     * @return word at that index in its natural form
+     */
+    @Override
+    public String getWord(int idx) {
+        return WORD_LIST[idx];
+    }
+
+    /**
+     * Gets the word at a given index in its scrambled form.
+     * @param idx index of required word
+     * @return word at that index in its scrambled form
+     */
+    @Override
+    public String getScrambledWord(int idx) {
+        return getScrambled(WORD_LIST[idx]).toLowerCase();
+    }
+    
+    private static String getScrambled(String s) {
+        String[] scram = s.split("");
+        List<String> letters = Arrays.asList(scram);
+        Collections.shuffle(letters);
+        StringBuilder sb = new StringBuilder(s.length());
+        for (String c : letters) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Gets the number of words in the library.
+     * @return the total number of plain/scrambled word pairs in the library
+     */
+    @Override
+    public int getSize() {
+        return WORD_LIST.length;
+    }
+
+    /**
+     * Checks whether a user's guess for a word at the given index is correct.
+     * @param idx index of the word guessed
+     * @param userGuess the user's guess for the actual word
+     * @return true if the guess was correct; false otherwise
+     */
+    @Override
+    public boolean isCorrect(int idx, String userGuess) {
+        return userGuess.toLowerCase().equals(getWord(idx).toLowerCase());
+    }
+
+}
